5b13d24ce093603895feed32325d959cde4c9b61
mmoramova <93440480+mmoramova@users.noreply.github.com>
2022-09-04 19:03:46 +0200

    Added dictionary API integration

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import logo from "./logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <main>
+            <Dictionary />
+          </main>
+          <footer className="App-footer">
+            <small>Coded by SheCodes</small>
+          </footer>
+        </header>
+      </div>
     </div>
   );
 }
